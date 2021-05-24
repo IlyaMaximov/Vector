179717b830dedef87edc485341ae5d5362ea3cc1
Илья Максимов <IlyaMaximovv@yandex.ru>
2021-05-24 19:44:06 +0300

    .gitignore were used

--- cmake-build-debug/lib/googletest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ilya/CLionProjects/Vector/lib/googletest/googlemock
-# Build directory: /home/ilya/CLionProjects/Vector/cmake-build-debug/lib/googletest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
